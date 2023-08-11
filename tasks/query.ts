e7311eb55be68c08bb743b08eba72792d1144d9f
adachi_440 <tomoki1999314@gmail.com>
2023-08-12 03:11:30 +0900

    fix cli scripts

--- tasks/query.ts
+++ tasks/query.ts
@@ -4,31 +4,46 @@ import { QueryType } from "../typechain-types/contracts/Gateway";
 import { BigNumber, ContractReceipt } from "ethers";
 import { GelatoRelay } from "@gelatonetwork/relay-sdk";
 import { MESSAGE } from "../test/utils/constants";
+import prettyjson from "prettyjson";
+// import ora from 'ora';
 
 const relay = new GelatoRelay();
 
 task(
   "TASK_QUERY",
   "execute a query",
-).addParam<string>("params",
-  'Parameters for requesting query\nExample: [{dstChainId: 5, height: 8000000, to: "0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43", slot: "0x0"}]',
-  "", types.string)
+).addParam<string>("callback", "CallBack address", "", types.string)
+  .addParam<string>("message", "Message", "", types.string)
+  .addParam<string>("params",
+    'Parameters for requesting query\nExample: [{dstChainId: 5, height: 8000000, to: "0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43", slot: "0x0"}]',
+    "", types.string)
   .setAction(async (taskArgs, hre): Promise<null> => {
+    let message = taskArgs.message,
+      callback = taskArgs.callback
+    if (message === "") {
+      message = MESSAGE
+    }
+    if (callback === "") {
+      callback = "0xda94E03f3c4C757bA2f1F7a58A00d2525569C75b" // Mock Receiver
+    }
     const params: QueryType.QueryRequestStruct[] = JSON.parse(taskArgs.params)
+
+    if (params.length === 0) {
+      throw new Error("Params is invalid")
+    }
 
     for (const param of params) {
       param.slot = hre.ethers.utils.hexZeroPad(param.slot.toString(), 32)
     }
 
-    console.log(`params: ${params}`)
+    console.log("params:")
+    console.log(prettyjson.render(params))
 
     const gatewayAddress = DEPLOYMENTS[hre.network.name as keyof typeof DEPLOYMENTS].gateway
     if (!gatewayAddress) throw new Error("Gateway address is not found")
     const gateway = await hre.ethers.getContractAt("Gateway", gatewayAddress)
 
-    const callBack = "0xda94E03f3c4C757bA2f1F7a58A00d2525569C75b" // Mock Receiver
     const lightClient = DEPLOYMENTS[hre.network.name as keyof typeof DEPLOYMENTS]["light_client"]
-    const message = MESSAGE
 
     const chainId = hre.network.config.chainId
     if (!chainId) throw new Error("Src Chain is invalid. Please check your network config")
@@ -36,13 +51,13 @@ task(
     const gasLimit = BigNumber.from("1000000")
 
     const fee = await relay.getEstimatedFee(chainId, nativeToken, gasLimit, true)
-    console.log("fee: ", fee.toString())
+    console.log("Query fee: ", fee.toString())
+    // let spinner = ora("Executing query...").start();
     try {
-      const tx = await gateway.query(params, lightClient, callBack, message, { gasLimit: 1000000, value: fee.mul(110).div(100) })
-      console.log("Executing query...")
+      const tx = await gateway.query(params, lightClient, callback, message, { gasLimit: 1000000, value: fee.mul(110).div(100) })
       const resTx: ContractReceipt = await tx.wait()
-      console.log(`The transaction is successful`)
-      console.log(tx)
+      // spinner.succeed("Transaction is executed successfully");
+      console.log(prettyjson.render(resTx))
       const events = resTx.events
       let queryId = ""
       if (events !== undefined) {
@@ -50,7 +65,7 @@ task(
       } else {
         throw new Error("QueryId is not found")
       }
-      console.log("Waitng for relaying and getting data...")
+      // spinner = ora("Waitng for relaying and getting data...").start();
       gateway.removeAllListeners()
       const filter = gateway.filters.ReceiveQuery(queryId, null, null, null, null)
 
@@ -58,17 +73,17 @@ task(
         try {
           gateway.on(filter, async (...args) => {
             const results = args[4]
-            console.log(`The query result is ${results}`)
+            // spinner.succeed(`The query result is ${results}`);
             resolve();
           })
         } catch (error) {
-          console.error(`Listener Failed: ${JSON.stringify(error)}`)
+          // spinner.fail(`Listener Failed: ${JSON.stringify(error)}`);
           reject(error)
         }
       })
 
     } catch (error) {
-      console.error(`The transaction is failed: ${JSON.stringify(error)}`)
+      // spinner.fail(`The transaction is failed: ${JSON.stringify(error)}`);
     }
     return null;
   });
